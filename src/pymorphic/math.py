9a7d9882fe394d8095d4da2744608f8576641ccb
sreea1408 <108213270+sreea1408@users.noreply.github.com>
2025-05-04 20:11:12 +0100

    Add math module

---
+++ src/pymorphic/math.py
@@ -0,0 +1,3 @@
+def add(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
---
+++ tests/test_math.py
@@ -0,0 +1,7 @@
+import pytest
+import pymorphic.math
+
+def test_add():
+    assert pymorphic.math.add(2, 3) == 5
+    assert pymorphic.math.add(-1, 1) == 0
+    assert pymorphic.math.add(0, 0) == 0
